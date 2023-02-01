e22dc6a5655b33cd721fc5fe58feb87bd3343aee
dennismayuga <dennismayu@gmail.com>
2023-02-01 17:42:30 -0500

    tested how to pull data from sql table into c# code

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // ...
+
+            string connectionString = "Data Source=DESKTOP-V50PKCU\\SQLEXPRESS;Initial Catalog=A1;Integrated Security=True;";
+            string sql = "select Price from dbo.productTable where Product_Name = 'Apple'";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            double temp = reader.GetDouble(0);
+
+                            Console.WriteLine(temp);
+                           // Console.WriteLine(reader["Price"]);
+                            
+                           
+
+                        }
+                        Console.ReadKey();
+                    }
+                }
+
+
+
+
+            }
+        }
+    }
+}
