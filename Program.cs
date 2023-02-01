6a714fe751af463722a48f154f0edd5e0e4603f6
dennismayuga <dennismayu@gmail.com>
2023-02-01 18:54:16 -0500

    detailing how to obtain specific values and adding it to a new cart table

--- Program.cs
+++ Program.cs
@@ -17,14 +17,14 @@ namespace TestingSQL
 
 
 
-            string fruit = "Raspberry";                 // input from textbox...
+            string fruit = "apple";                 // input from textbox...
             decimal weight = Convert.ToDecimal(12);     // input from textbox...
 
 
 
 
             string connectionString = "Data Source=DESKTOP-V50PKCU\\SQLEXPRESS;Initial Catalog=A1;Integrated Security=True;";
-            string sql = "select Price from dbo.productTable where Product_Name = '"+fruit+"'";
+            string sql = "select * from dbo.productTable where Product_Name = '"+fruit+"'";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -37,10 +37,39 @@ namespace TestingSQL
                         while (reader.Read())
                         {
 
-                            double temp = reader.GetDouble(0);
+                            //double temp = reader.GetDouble(0);
+                            //decimal price = Convert.ToDecimal(temp);
+                            //decimal finalPrice = weight * price;
+                            //Console.WriteLine("your final price : " + finalPrice);
+
+                            Console.WriteLine(reader["Product_ID"] + " " + reader["Product_Name"] + " " + reader["KG"] + " " + reader["Price"]);
+
+                            
+                            double temp = Convert.ToDouble(reader["Price"]);
                             decimal price = Convert.ToDecimal(temp);
                             decimal finalPrice = weight * price;
-                            Console.WriteLine("your final price :" + finalPrice);
+                            Console.WriteLine("your final price : " + finalPrice);
+
+
+
+                           
+                           string sql = "INSERT into dbo.Cart values(@Product_ID, @Product_Name,@KG,@Price";
+
+                            SqlCommand command1 = new SqlCommand(sql, connection);
+                           command.Parameters.AddWithValue("@Product_ID", reader["Product_ID"]);
+                           command.Parameters.AddWithValue("@Product_Name", reader["Product_Name"]);
+                           command.Parameters.AddWithValue("@KG", weight);
+                           command.Parameters.AddWithValue("@Price", reader["Price"]);
+
+
+                            string sql = "select *";
+                            SqlCommand command2 = new SqlCommand(sql, connection);
+                            Console.WriteLine(reader["Product_ID"] + " " + reader["Product_Name"] + " " + reader["KG"] + " " + reader["Price"]);
+
+
+
+
+
 
 
 
