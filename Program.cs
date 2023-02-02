6962a1e85bb1d536c67960f51322d889fcc99390
dennismayuga <dennismayu@gmail.com>
2023-02-02 16:31:17 -0500

    created code block to check sql inventory and compare to input

--- Program.cs
+++ Program.cs
@@ -24,8 +24,9 @@ namespace TestingSQL
 
 
             string connectionString = "Data Source=DESKTOP-V50PKCU\\SQLEXPRESS;Initial Catalog=A1;Integrated Security=True;MultipleActiveResultSets=True";
-            //string sql = "select * from dbo.productTable where Product_Name = '"+fruit+"'";
-            string sql = "select * from dbo.cartTable";
+            string sql = "select KG from dbo.productTable where Product_Name = '"+fruit+"'";
+            //string sql = "select * from dbo.cartTable";
+            
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -43,7 +44,7 @@ namespace TestingSQL
                             //Console.WriteLine("your final price : " + finalPrice);
 
 
-
+                            // USED TO GRAB SPECIFIC VALUES
 
                             /*
                             Console.WriteLine(reader["Product_ID"] + " " + reader["Product_Name"] + " " + reader["KG"] + " " + reader["Price"]);
@@ -61,26 +62,52 @@ namespace TestingSQL
                             */
 
 
-                           
-                         
 
-                          
 
+
+
+                            // USED TO CREATE SPECIFIC QUERIES
+
+                            /*
                                 string productName = Convert.ToString(reader["Product_Name"]);
                                 int kg = (int)Convert.ToInt64(reader["KG"]);
 
                                 Console.WriteLine(productName + "   "+ kg );
                                 string sqlq = "UPDATE dbo.productTable SET KG = KG + " + kg + " where Product_Name = '"+productName.TrimEnd()+"'";
-                                //string sqlquery = "UPDATE dbo.productTable SET KG = KG + @kg where Product_Name = '"+productName.TrimEnd()+"'";
+                              
 
                                
 
                                 Console.WriteLine(sqlq);
                                 SqlCommand command2 = new SqlCommand(sqlq, connection);
                                 command2.ExecuteNonQuery();
-                               
+                              */
+
+                            
+                            int inputKG = 31;
+                            int checkKG = (int)Convert.ToInt64(reader["KG"]);
+                            if (inputKG > checkKG )
+                            {
+                                
+                                Console.WriteLine("Cannot be done because inventory too low...REQUEST = "+inputKG  +", actually INVENTORY = "+checkKG);
+                            } else
+                            {
+                                /*
+                                string query = "Insert into cartTable values(@productID, @productName,  @price, @amountKG) "; //We need to call the textBox and then grab the text
+                                SqlCommand cmd = new SqlCommand(query, connection);
+                                cmd.Parameters.AddWithValue("@productID", int.Parse(productID.Text));
+                                cmd.Parameters.AddWithValue("@productName", productName.Text);
+                                cmd.Parameters.AddWithValue("@price", float.Parse(price.Text));
+                                cmd.Parameters.AddWithValue("@amountKG", int.Parse(amountKG.Text));
 
 
+                                //We now need to execute our query
+                                cmd.ExecuteNonQuery();
+                                */
+                                Console.WriteLine("DONE because REQUEST = "+inputKG  +", actually INVENTORY = "+checkKG);
+                            }
+
+                           
 
 
 
@@ -90,6 +117,7 @@ namespace TestingSQL
 
 
                         }
+                        connection.Close();
                         Console.ReadKey();
                     }
                 }
